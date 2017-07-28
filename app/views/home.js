aafe36aec2461b7253c562e2944fb8a77d6bc63d
Dylan Piercey <pierceydylan@gmail.com>
2017-07-28 10:43:35 -0700

    Update deps

--- app/views/home.js
+++ app/views/home.js
@@ -3,10 +3,6 @@ import logoImg from '../assets/logo.jpg'
 
 export default async ({ res, locals }) => {
   locals.title = 'Home Page'
-
-  setTimeout(() => {
-    throw new Error('fail')
-  }, 100)
   res.body = (
     <div>
       <img src={logoImg} />
